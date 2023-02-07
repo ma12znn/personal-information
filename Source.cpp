81c5c4604fa12bd8850c104082240a5ad8db7a5f
ma12znn <124689463+ma12znn@users.noreply.github.com>
2023-02-07 17:29:16 +0300

    Add files via upload

---
+++ Source.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	string name;
+	string favoritecolor;
+	int age;
+	double weight;
+	cout << "what is your name:" << endl;
+	cin >> name;
+	cout << "How old are you" <<endl;
+	cin >> age;
+	cout << "what is your favorite color" << endl;
+	cin >> favoritecolor;
+	cout << "How much do you weigh" << endl;
+	cin >> weight;
+	system("pause");
+	return 0;
+}
